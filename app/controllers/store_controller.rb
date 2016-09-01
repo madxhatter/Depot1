7f06ed04bf85046fd5179e762bafdea6ef7ca626
Gabriel Ramos <gabelr90@gmail.com>
2016-09-01 14:55:55 -0500

    pre ch 10

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
+    def increment_count
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
+    end
+
+    @count = increment_count
     @products = Product.order(:title)
   end
 end
